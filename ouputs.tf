cc0a011e456ab0b0a9af82b04348550447761675
kriiishamtic <kriiishmatic@gmail.com>
2025-10-27 16:04:16 +0530

    Your commit message

--- ouputs.tf
+++ ouputs.tf
@@ -1,11 +1,6 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
-
-output "internet_gateway_id" {
-  value = aws_internet_gateway.main.id
-  
-}
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
